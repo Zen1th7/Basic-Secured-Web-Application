bc047cfd08c56b94efe5ad373cf561fd08491a4f
Zenith <andrian.wu.7@gmail.com>
2024-11-14 16:50:38 +0800

    Modified login.html and erased javascript

--- src/main/resources/templates/enter_login.js
+++
@@ -1,6 +0,0 @@
-function focusPassword(event) {
-    if (event.key === "Enter") {
-        event.preventDefault(); // Prevent form submission
-        document.getElementById("password").focus(); // Move focus to password field
-    }
-}
